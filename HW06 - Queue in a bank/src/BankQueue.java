c06d2cae69ad49277f2a4b2b51364bd4af90c11e
oatkup1a <voraphol12345@gmail.com>
2024-02-29 11:24:27 +0700

    Add files via upload

---
+++ HW06 - Queue in a bank/src/BankQueue.java
@@ -0,0 +1,17 @@
+
+public class BankQueue { // must work for any implementation of DeQ
+	DeQ[] counters;
+	DeQ special;
+
+	public BankQueue(DeQ[] counters, DeQ special) {
+		super();
+		this.counters = counters;
+		this.special = special;
+	}
+
+	//Write this method
+	public void distribute() throws Exception {
+		
+	}
+
+}
